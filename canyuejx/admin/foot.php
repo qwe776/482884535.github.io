58ea73137915e2a7d12e000f3a51ff67d090b51f
482884535 <76425856+482884535@users.noreply.github.com>
2022-02-01 21:52:44 +0800

    Delete canyuejx directory

--- canyuejx/admin/foot.php
+++
@@ -1,3 +0,0 @@
-<div id="footer"><a href="/" target="_blank" style="color:#999999">Copyright © 2021 Sillycat All Rights Reserved</span>
-			<br/></a>
-</div> 
--- canyuejx/admin/inc.php
+++
@@ -1,5 +0,0 @@
-<title>残月解析管理后台</title>
-<meta name="keywords" content="残月解析平台" />
-<meta name="description" content="残月解析平台" />
-<link href="./images/admin.css" rel="stylesheet" type="text/css" />
-<link rel="shortcut icon" type="image/x-icon" href="./../favicon.ico"><?php 
