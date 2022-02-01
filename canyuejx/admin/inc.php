34764ba0466af81abc65fb27b0d2c88a7b7a03f5
482884535 <76425856+482884535@users.noreply.github.com>
2022-02-01 21:21:48 +0800

    Add files via upload

---
+++ canyuejx/admin/inc.php
@@ -0,0 +1,5 @@
+<title>残月解析管理后台</title>
+<meta name="keywords" content="残月解析平台" />
+<meta name="description" content="残月解析平台" />
+<link href="./images/admin.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" type="image/x-icon" href="./../favicon.ico"><?php 
